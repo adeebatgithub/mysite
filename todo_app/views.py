db80631e85c35959119a78ab7fd330fef907f057
adeebatgithub <7356064624ad@gmail.com>
2023-11-26 11:02:03 +0530

    update: dashboard

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.views import View
 from django.views import generic as gv
-from django.contrib.auth import mixins as mx
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-class Index_View(mx.LoginRequiredMixin, gv.RedirectView):
+class Index_View(LoginRequiredMixin, gv.RedirectView):
     
     url = "/todo/dashboard"
+
+class Dashboard_View(LoginRequiredMixin, gv.TemplateView):
+
+    template_name = "dashboard.html"
